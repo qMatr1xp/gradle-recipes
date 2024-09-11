1ea60dcd61a11cd54d7e0417317651fc4e450836
Micah Jo <micahjo@google.com>
2024-09-11 16:56:21 -0700

    Update Gradle in AGP to 8.10.2
    Bug: n/a
    Test: n/a
    Change-Id: I1752c3cfa648a25b9892c2ed2b5c8edfab30423d

--- recipes/asmTransformClasses/build-logic/plugins/build.gradle.kts
+++ recipes/asmTransformClasses/build-logic/plugins/build.gradle.kts
@@ -28,7 +28,7 @@ java {
 dependencies {
     compileOnly(libs.android.gradlePlugin.api)
     implementation(gradleKotlinDsl())
-    implementation("org.ow2.asm:asm-util:9.6")
+    implementation("org.ow2.asm:asm-util:9.7")
 }
 
 gradlePlugin {
